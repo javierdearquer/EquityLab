a53b0411077bb7fcd85dffa3d315ba3c1b9b0ca6
Javier <javierdearquer@gmail.com>
2025-04-26 18:59:43 +0200

    Exception control for items not appearing in BS

--- DCF_valuation_v3.py
+++ DCF_valuation_v3.py
@@ -12,16 +12,15 @@ Parameters:
 4. Terminal growth
 5. WC as % of revenue
 '''
-n_periods_growth = 8
-n_periods_transition = 3
+ticker_str = ""
 
-growth_initial = 0.125 #TODO estimate from the historical data
+n_periods_growth = 2
+n_periods_transition = 8
+
+growth_initial = 0.065 #TODO estimate from the historical data
 growth_terminal = 0.025 #TODO potentially could be LLM generated based on news and internet research on the field
 
 start_year = 2025 # TODO take the next year after the last of financials
-
-wc_of_revenue = 0.127
-
 
 def dcf(cash_flows, discount_rate):
     # Calculate the present value using: PV = CF / (1 + r)^t + TV/(1 + r)^T
@@ -32,12 +31,12 @@ def get_risk_free_rate():
     """Fetches the US 10-Year Treasury yield from yfinance."""
     try:
         treasury = yf.Ticker("^TNX")  # ^TNX = CBOE 10-Year Treasury Note Yield
-        yield_percent = treasury.history(period="1d")["Close"].iloc[-1]
-        return yield_percent / 100  # Convert from % to decimal
+        data = treasury.history()
+        last_quote = data['Close'].iloc[-1]
+        return last_quote / 100  # Convert from % to decimal
     except:
         return 0.04  # fallback default
     
-ticker_str = "AAPL"
 ticker = yf.Ticker(ticker_str)
 
 info = ticker.info
@@ -45,27 +44,45 @@ beta = info.get("beta", None)
 
 # Divided to get all units in millions
 fin_is = ticker.quarterly_financials.T/1000000
+#fin_is = ticker.financials.T/1000000
 fin_is = fin_is.sort_index()
 
 fin_bs = ticker.quarterly_balancesheet.T/1000000
+#fin_bs = ticker.balancesheet.T/1000000
 fin_bs = fin_bs.sort_index()
 
 fin_cf = ticker.quarterly_cashflow.T/1000000
+#fin_cf = ticker.cashflow.T/1000000
 fin_cf = fin_cf.sort_index()
 
 # TODO convert all the numerical values to the same format (i.e. float with 2 decimals)
-# TODO determine if I want the last value of the mean , ...
+# TODO determine if I want the last value of the mean ...
 
 # Compute the TTM revenue of the firm (based on the 10Q filings)
 revenue = fin_is['Total Revenue'][-4:].sum(axis=0)
 
 earnings = fin_is['Net Income'][-4:].sum(axis=0)
-dividends = abs(fin_cf['Cash Dividends Paid'][-4:].sum(axis=0))
-capex = abs(fin_bs['Gross PPE'].diff().fillna(0).iloc[-1]) # Gross - Depreciation = Net PPE -> investment is Delta on Gross
+#dividends = abs(fin_cf['Cash Dividends Paid'][-4:].sum(axis=0))
+try:
+    capex = abs(fin_bs['Gross PPE'].diff().fillna(0).iloc[-1]) # Gross - Depreciation = Net PPE -> investment is Delta on Gross
+except:
+    capex = abs(fin_bs['Net PPE'].diff().fillna(0).iloc[-1]) # Gross - Depreciation = Net PPE -> investment is Delta on Gross
+
 depreciation = abs(fin_cf['Depreciation And Amortization'][-4:].sum(axis=0))
 
-wc = fin_bs['Working Capital']
-dwc = fin_bs['Working Capital'].diff().fillna(0) # change in working capital    
+try:
+    wc = fin_bs['Working Capital']
+except:
+    try:
+        wc = fin_bs['Current Assets'] - fin_bs['Current Liabilities'] 
+    except:
+        wc = fin_bs.iloc[:,0]-fin_bs.iloc[:,0]
+
+dwc = wc.diff().fillna(0) # change in working capital    
+
+wc_of_revenue = (wc)/fin_is['Total Revenue'] # Get latest WC values and divide by the Total Revenue
+wc_of_revenue = wc_of_revenue.mean() # make the average of all of them
+
 debt_to_equity = fin_bs['Total Debt'] / fin_bs['Stockholders Equity'] # debt to equity ratio
 debt_to_equity = debt_to_equity.mean()
 
@@ -106,8 +123,8 @@ TV = df['FCFE'].iloc[-1:]*(1+growth_terminal)/(cost_of_equity-growth_terminal)
 PV_TV = TV.iloc[0] / (1+cost_of_equity)**(n_periods_growth+n_periods_transition)
 
 n_shares = fin_bs['Ordinary Shares Number'].iloc[-1] # number of shares outstanding
-
-print(f"Share Price: {ticker.history(period="1d")["Close"].iloc[-1]}")
+share_price = ticker.history()["Close"].iloc[-1]
+print(f"Share Price: {share_price}")
 print(f"Valuation: {(PV_FCFE+PV_TV)/n_shares}")
 
 #print(df)
